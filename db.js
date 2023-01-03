5f8609f36b9a26214ecde3dbb0b2e8982068f6be
Bharathi.Valluri <you@example.com>
2023-01-03 12:21:37 +0530

    modified

--- db.js
+++ db.js
@@ -11,30 +11,30 @@ client.connect(function (err) {
   if (err) throw err
   console.log('Connected!')
 
-  //   var sqlQuery = `CREATE TABLE Managers(
-  //       m_id INT NOT NULL,
-  //       m_name VARCHAR(255) NOT NULL,
-  //       m_age INT NOT NULL,
-  //       PRIMARY KEY(m_id)
-  //       );
+  var sqlQuery = `CREATE TABLE Managers(
+        m_id INT NOT NULL,
+        m_name VARCHAR(255) NOT NULL,
+        m_age INT NOT NULL,
+        PRIMARY KEY(m_id)
+        );
 
-  //       CREATE TABLE Employees(
-  //       emp_id INT NOT NULL,
-  //       emp_name VARCHAR(255) NOT NULL,
-  //       emp_age INT NOT NULL,
-  //       PRIMARY KEY (emp_id)
-  //      );
+        CREATE TABLE Employees(
+        emp_id INT NOT NULL,
+        emp_name VARCHAR(255) NOT NULL,
+        emp_age INT NOT NULL,
+        PRIMARY KEY (emp_id)
+       );
 
-  //      CREATE TABLE manager_employee (
-  //         manager_id INT NOT NULL,
-  //         employee_id INT NOT NULL,
-  //         PRIMARY KEY (manager_id, employee_id),
-  //         FOREIGN KEY(manager_id) REFERENCES Managers (m_id),
-  //         FOREIGN KEY(employee_id) REFERENCES employees (emp_id)
-  //      );
-  //  `
-  //   client.query(sqlQuery)
-  //   console.log('created!')
+       CREATE TABLE manager_employee (
+          manager_id INT NOT NULL,
+          employee_id INT NOT NULL,
+          PRIMARY KEY (manager_id, employee_id),
+          FOREIGN KEY(manager_id) REFERENCES Managers (m_id),
+          FOREIGN KEY(employee_id) REFERENCES employees (emp_id)
+       );
+   `
+  client.query(sqlQuery)
+  console.log('created!')
 })
 
 module.exports = { client }
